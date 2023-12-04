25eb54c9c3671f63fd50b8c34fab3f1aa16f5bfd
Damian Olszewski <OleD@viessmann.com>
2023-12-04 20:37:03 +0100

    move files

--- ConsoleAppTests/UnitTest1.cs
+++ ConsoleAppTests/UnitTest1.cs
@@ -8,8 +8,32 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
+        public void TestOfInt()
+        {  //Arrange
+            int number1 = 2;
+            int number2 = 2;
+            // Act
+            //Assert
+            Assert.That(number2, Is.EqualTo(number1));
+        }
+
+        [Test]
+        public void TestOfFloat()
+        {  //Arrange
+            float number1 = 12345678901234567890;
+            float number2 = 12345678901234567890;
+            // Act
+            //Assert
+            Assert.That(number2, Is.EqualTo(number1));
+        }
+
+        [Test]
+        public void TestOfString()
+        {  //Arrange
+            string name1 = "Adam";
+            string name2 = "Adam";
+            // Act
+            //Assert
+            Assert.That(name2, Is.EqualTo(name1));
+        }
 }
